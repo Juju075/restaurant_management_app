b3a1ad49cbbc66199af220c14cf8d8335ca42471
elgato <amzfba.1bestbuy@gmail.com>
2022-07-17 14:39:30 +0200

    main: works fine

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).use(router).mount('#app')
+/**
+ * 3. Create the router instance and pass the `routes` option
+ */
+import router from "./router";
+
+// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+
+// 5. Create and mount the root instance.
+/**
+ * const app = Vue.createApp({})
+ * app.use(router)
+ * app.mount('#app')
+ */
+createApp(App).use(router).mount("#app");
